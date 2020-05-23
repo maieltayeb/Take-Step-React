be1849a83e3abad32f29c4e8839d58b765947e59
NadaYoussef-d <59627123+NadaYoussef-d@users.noreply.github.com>
2020-05-23 11:40:11 +0200

    login,portflio,businnessOwnerprofileCard

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
-import Header from "./Layout/Header/header"
+import Header from "./Layout/Header/header";
 import WelcomeNavBar from "./Layout/Navbar/navbar";
 import ModalPost from "./Components/Modal/AddPostModel/modal";
 import AddSkill from "./Components/Modal/AddSkillModal/AddSkillModal";
-import Post from './Components/post/post';
+import Post from "./Components/post/post";
+import Login from "./Pages/LoginPage/login";
+import Portflio from "./Components/Portflio/Portflio";
+import OwnerProfileCard from "./Components/Card/BusinessOwnerProfileCard/OwnerProfileCard";
 // import EducationSection from './Components/EducationSection/EducationSection';
 // import CreateTask from './Pages/HomePage/CreateTaskSection/createTask';
 ReactDOM.render(
   <React.StrictMode>
-  {/* <WelcomeNavBar />
+    {/* <WelcomeNavBar />
   <Header/>
   <AddSkill/>
   <ModalPost/>
@@ -22,16 +25,20 @@ ReactDOM.render(
 
     {/* <App /> */}
     {/* <App /> */}
-   
-   {/* <CreateTask  /> */}
-  {/* <WelcomeNavBar />
+
+    {/* <CreateTask  /> */}
+    {/* <WelcomeNavBar />
     <ModalPost />
     <Post/> */}
-  {/* <EducationSection />  */}
+    {/* <EducationSection />  */}
 
     {/* <App /> */}
     {/* <App /> */}
     {/* <Header/> */}
+
+    {/* <Login /> */}
+    {/* <Portflio /> */}
+    {/* <OwnerProfileCard /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
