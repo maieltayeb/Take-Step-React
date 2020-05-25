9bac2c2d48fc82144e0dfcad7e838871dd570d1b
NadaYoussef <yousefnada95@gmail.com>
2020-05-25 15:36:29 +0200

    portflio change trail

--- src/Components/Portflio/Portflio.js
+++ src/Components/Portflio/Portflio.js
@@ -21,12 +21,15 @@ const Portflio = props => {
         <div className="Portflio-item">
           <img
             className="Portflio-item-img"
-            style={{ borderRadius: " 50% 0" }}
+            style={{
+              borderRadius: " 50% 0"
+            }}
             top
             width="100%"
             src="./img/watch.jpg"
             alt="Card image cap"
           />
+
           <div className="Portflio-item-title">Card title</div>
         </div>
       </div>
@@ -35,7 +38,6 @@ const Portflio = props => {
         See More
         <i class="fas fa-chevron-right mt-1" style={{ paddingLeft: "5px" }}></i>
       </div>
-      <div>hi</div>
     </div>
   );
 };
