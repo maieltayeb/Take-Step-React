43dfa8038b96f97a2b44f6439cf6cb614f982fc4
NadaYoussef <yousefnada95@gmail.com>
2020-05-28 20:52:18 +0200

    cc

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
       {/* <HowITWork></HowITWork> */}
       {/* <Example /> */}
       {/* <HowItWorkCard></HowItWorkCard> */}
-      <VolunteerProfile></VolunteerProfile>
+      {/* <VolunteerProfile></VolunteerProfile> */}
       {/* <BussinessOwnerProfile></BussinessOwnerProfile> */}
       {/* <Footer></Footer> */}
       {/* <AddEducation /> */}
