7fc8f49e0203fefe8c4f1a23dca2f0410ac39ebd
hala <HalaNazmy12@gmail.com>
2020-05-29 01:09:00 +0200

    update task-submitted-page

---
+++ src/Components/Task-submitted/Task-submitted.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ProjectLink from "../ProjectLink/project-Link-Component";
+const TaskSubmit = () => {
+  return (
+    <div>
+      <div className="poject-title ">
+        <div className="project-title-container">
+          <p className="project-title-container-p">Project-Title</p>
+        </div>
+        <div className="project-description">
+          <p className="project-description-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+          </p>
+        </div>
+        <ProjectLink />
+        <ProjectLink />
+        <ProjectLink />
+        <ProjectLink />
+      </div>
+    </div>
+  );
+};
+
+export default TaskSubmit;
