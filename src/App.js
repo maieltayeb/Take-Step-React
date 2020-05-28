cb028698f5cb1de96669d8a492fdeb85737af24a
NadaYoussef <yousefnada95@gmail.com>
2020-05-28 11:21:19 +0200

    app commented

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <div>
       {/* <Welcome></Welcome> */}
       {/* <Example /> */}
-      <NotificationDropdown />
+      {/* <NotificationDropdown /> */}
       {/* <Login></Login> */}
       {/* <SignUp /> */}
       {/* <Portflio /> */}
