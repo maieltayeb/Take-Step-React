6b42350f6223e5c884bffd1a0af86db73a4db3f3
NadaYoussef <yousefnada95@gmail.com>
2020-05-28 21:26:16 +0200

    modal trail

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
       <ModalCreateTask />
       <FeedbackModel /> */}
       {/* <TaskDetails /> */}
-      <TaskSubmittedFrame />
+      {/* <TaskSubmittedFrame /> */}
     </div>
   );
 }
