683d6ca1312a0e3f3f9165c968e6d6b5fe20e74f
Mariam Magdy <magdymariam@gmail.com>
2020-06-13 08:48:26 +0200

    handle to show another div when there is no jobs

--- src/Components/Task/taskprofile.js
+++ src/Components/Task/taskprofile.js
@@ -17,58 +17,62 @@ const TaskProfile = props => {
 
   return (
     <>
-      {jobsCurrentUserState.map(item => (
-        <div className="profile-postContainer m-4 rounded">
-          <JobProfile job={item}></JobProfile>
+      {jobsCurrentUserState.length ? (
+        jobsCurrentUserState.map(item => (
+          <div className="profile-postContainer m-4 rounded">
+            <JobProfile job={item}></JobProfile>
 
-          <div className="profile-postCommentBody shadow-sm p-4 mb-4 bg-white">
-            <div className=" profile-reactToPost ml-2 clearfix">
-              <div className=" ml-3 float-left">
-                <span>Like</span>
-                <span>
-                  <i className=" ml-3  mb-3 fas fa-thumbs-up"></i>
-                </span>
+            <div className="profile-postCommentBody shadow-sm p-4 mb-4 bg-white">
+              <div className=" profile-reactToPost ml-2 clearfix">
+                <div className=" ml-3 float-left">
+                  <span>Like</span>
+                  <span>
+                    <i className=" ml-3  mb-3 fas fa-thumbs-up"></i>
+                  </span>
+                </div>
+                <div className=" ml-4 float-left">
+                  <span>Comment</span>
+                  <span>
+                    <i className=" ml-3  mb-3 fas fa-comment-alt"></i>
+                  </span>
+                </div>
               </div>
-              <div className=" ml-4 float-left">
-                <span>Comment</span>
-                <span>
-                  <i className=" ml-3  mb-3 fas fa-comment-alt"></i>
-                </span>
+              <div className="clearfix d-flex">
+                <div className=" float-left ">
+                  <img className=" img rounded-circle" src="/img/people.png" />
+                </div>
+                <div className=" ml-2 float-left ">
+                  <Input
+                    placeholder="Add your comment"
+                    className="mt-3 profile-commentArea"
+                    style={{
+                      width: "550px",
+                      border: " 1px solid #ebc010",
+                      borderRadius: "50px"
+                    }}
+                  ></Input>
+                </div>
               </div>
-            </div>
-            <div className="clearfix d-flex">
-              <div className=" float-left ">
-                <img className=" img rounded-circle" src="/img/people.png" />
-              </div>
-              <div className=" ml-2 float-left ">
-                <Input
-                  placeholder="Add your comment"
-                  className="mt-3 profile-commentArea"
-                  style={{
-                    width: "550px",
-                    border: " 1px solid #ebc010",
-                    borderRadius: "50px"
-                  }}
-                ></Input>
-              </div>
-            </div>
-            <div className="clearfix d-flex">
-              <div className=" float-left ">
-                <img
-                  className="img mt-2 rounded-circle"
-                  src="/img/people.png"
-                />
-              </div>
-              <div className=" ml-2 float-left ">
-                <div className="p-2 mt-2 profile-commentbody">
-                  <p className=" m-1">Aya Rabea</p>
-                  <p className=" m-1 small">My comment here...</p>
+              <div className="clearfix d-flex">
+                <div className=" float-left ">
+                  <img
+                    className="img mt-2 rounded-circle"
+                    src="/img/people.png"
+                  />
+                </div>
+                <div className=" ml-2 float-left ">
+                  <div className="p-2 mt-2 profile-commentbody">
+                    <p className=" m-1">Aya Rabea</p>
+                    <p className=" m-1 small">My comment here...</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <div className="profile-postCommentBody  p-4 mb-4">No Jobs to show</div>
+      )}
     </>
   );
 };
