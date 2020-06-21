ae7ac7a6c7a65293fe2c81eaa23d36d0f168ede0
hala <HalaNazmy12@gmail.com>
2020-06-21 16:29:45 +0200

    comment with profile owner

--- src/Components/Task/taskprofile.js
+++ src/Components/Task/taskprofile.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Input } from "reactstrap";
 import "./taskprofile.css";
 import JobProfile from "./jobProfile";
+import AddComment from "../post/addComment";
+import Comment from "../post/comment";
 import { connect } from "react-redux";
 import profilePic from "../../profileImage/profilephoto.png";
 
@@ -20,74 +22,66 @@ const TaskProfile = props => {
   return (
     <>
       {jobsCurrentUserState.length ? (
-        jobsCurrentUserState.map(item => (
-          <div className="profile-postContainer m-4 rounded">
-            <JobProfile job={item}></JobProfile>
+        jobsCurrentUserState.map(item => {
+          let realComments = [];
+          if (item.comments) {
+            for (let i = 0; i < Object.keys(item.comments).length; i++) {
+              const element = item.comments[Object.keys(item.comments)[i]];
+              realComments.push(element);
+            }
+            return (
+              <>
+                <div className="profile-postContainer m-4 rounded">
+                  <JobProfile job={item}></JobProfile>
 
-            <div className="profile-postCommentBody shadow-sm p-4 mb-4 bg-white">
-              <div className=" profile-reactToPost ml-2 clearfix">
-                {/* <div className=" ml-3 float-left">
-                  <span>Like</span>
-                  <span>
-                    <i className=" ml-3  mb-3 fas fa-thumbs-up"></i>
-                  </span>
-                </div> */}
-                <div className="float-left">
-                  <span>
-                    <i class=" m-1  mr-2 fas fa-comment-alt"></i>
-                  </span>
-                  <span>Comment</span>
-                </div>
-              </div>
-              <div className="clearfix d-flex">
-                <div className=" float-left ">
-                  {currentUser.imgUrl ? (
-                    <img
-                      src={"http://localhost:4402/" + currentUser.imgUrl}
-                      className="post-img  rounded-circle"
-                      alt="server pic"
-                      style={{ marginTop: "10px" }}
-                    />
-                  ) : (
-                    <img
-                      src={profilePic}
-                      // src={profilePic}
-                      className=" img rounded-circle"
-                      alt="personal pic"
-                      style={{ marginTop: "10px" }}
-                    />
-                  )}
-                  {/* <img className=" img rounded-circle" src="/img/people.png" /> */}
-                </div>
-                <div className=" ml-2 float-left ">
-                  <Input
-                    placeholder="Add your comment"
-                    className="mt-3 profile-commentArea"
-                    style={{
-                      width: "550px",
-                      border: " 1px solid #ebc010",
-                      borderRadius: "50px"
-                    }}
-                  ></Input>
-                </div>
-              </div>
-              <div className="clearfix d-flex">
-                <div className=" float-left ">
-                  <img
-                    className="img mt-2 rounded-circle"
-                    src="/img/people.png"
-                  />
-                </div>
-                <div className=" ml-2 float-left ">
-                  <div className="p-2 mt-2 profile-commentbody">
-                    <p className=" m-1">Aya Rabea</p>
-                    <p className=" m-1 small">My comment here...</p>
+                  <div className="profile-postCommentBody shadow-sm p-4 mb-4 bg-white">
+                    <div className=" profile-reactToPost ml-2 clearfix">
+                      <div className="float-left">
+                        <span>
+                          <i class=" m-1  mr-2 fas fa-comment-alt"></i>
+                        </span>
+                        <span>Comment</span>
+                      </div>
+                    </div>
+                    <AddComment jobId={item.id} />
+                    {realComments.length ? (
+                      realComments.map(comment => {
+                        return (
+                          <Comment
+                            key={Comment.id}
+                            comment={comment}
+                            jobId={item.id}
+                          />
+                        );
+                      })
+                    ) : (
+                      <div className="ml-3 mt-3">{item.id}</div>
+                    )}
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        ))
+              </>
+            );
+          } else {
+            return (
+              <>
+                <div className="profile-postContainer m-4 rounded">
+                  <JobProfile job={item}></JobProfile>
+                  <div className="postCommentBody shadow-sm p-4 mb-8 bg-white">
+                    <div className=" reactToPost ml-2 mb-0 clearfix">
+                      <div className="float-left">
+                        <span>
+                          <i class=" m-1  mr-2 fas fa-comment-alt"></i>
+                        </span>
+                        <span>Comment</span>
+                      </div>
+                    </div>
+                    <AddComment jobId={item.id} />
+                  </div>
+                </div>
+              </>
+            );
+          }
+        })
       ) : (
         <div className="profile-postCommentBody  p-4 mb-4">No Jobs to show</div>
       )}
